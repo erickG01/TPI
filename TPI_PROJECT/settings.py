c29689120e8219f1d6c8f3d7f41302ea75a6b863
Erick Girón <erickchelote@gmail.com>
2024-12-22 17:31:49 -0600

    subiendo nuevos cambios

--- TPI_PROJECT/settings.py
+++ TPI_PROJECT/settings.py
@@ -10,10 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
-import os
+
 from pathlib import Path
 import dj_database_url
 from dotenv import load_dotenv
+import os
 
 load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
